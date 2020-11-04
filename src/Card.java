e7821677dfedb26e9305f77edb932c731e57e4fe
mrAdamTweedie <61285048+AdamTweedie@users.noreply.github.com>
2020-11-04 12:24:13 +0000

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,13 @@
+/**
+ * Card constructors. This class's sole job is to initialise
+ * the newly created object and to have an instance method to
+ * return objects.
+ *
+ * @author 690024916 & 690023094
+ * @version 1.0
+ */
 public class Card {
+    // Each card has a face value of a non-negative integer
     private final short value;
 
     public Card(short value) {
