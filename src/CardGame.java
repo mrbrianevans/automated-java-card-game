c7ba8d013977e2c274668b4dfb4c262b920dd6d8
mrAdamTweedie <61285048+AdamTweedie@users.noreply.github.com>
2020-11-04 12:26:35 +0000

    Update CardGame.java
    Added comments and documentation

--- src/CardGame.java
+++ src/CardGame.java
@@ -2,9 +2,18 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+/**
+ * CardGame - initialises threads for all players. Also controls the logic of the game
+ * and specifies the setting up of all the cards (through distributing of cards to players).
+ * This class also will inform the relevant methods/threads when a player has won.
+ *
+ * @author 690024916 & 690023094
+ * @version 1.0
+ */
 public class CardGame {
     public static void main(String[] args) throws IOException {
-        // request from command line
+        // Request from command line.
         System.out.println("Enter the number of players in the game: ");
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(in.readLine());
@@ -17,16 +26,16 @@ public class CardGame {
             CardDeck[] decks = new CardDeck[n];
 
             for (short i = 1; i <= n; i++) {
-                players[i - 1] = new Player(i); // creating thread because Player extends Thread
+                players[i - 1] = new Player(i); // Creating thread because Player extends Thread.
                 players[i - 1].start();
                 decks[i - 1] = new CardDeck(i);
             }
 
-            // distribute the cards to players
+            // Distribute the cards to players.
             for (int i = 0; i < 4 * n; i++) {
                 players[i % n].addCard(cardsInPack[i]);
             }
-            // distribute the cards to decks
+            // Distribute the cards to decks.
             for (int i = 4 * n; i < 8 * n; i++) {
                 decks[i % n].addCard(cardsInPack[i]);
             }
@@ -61,10 +70,10 @@ public class CardGame {
                     winner = players[playersTurn].getPlayerNumber();
                 }
             }
-            // procedure for when there is a winner
 
-            // inform all players of winner
-            // inform decks of end of game
+            // Procedure for when there is a winner.
+            // Inform all players of winner.
+            // Inform decks of end of game.
             for (short i = 0; i < n; i++) {
                 synchronized (players[i]) {
                     players[i].informPlayerHasWon(winner);
@@ -72,10 +81,10 @@ public class CardGame {
                 decks[i].writeContentsToFile();
             }
         } catch (FileNotFoundException e) {
-            System.out.println("File not found");
+            System.out.println("File not found.");
         } catch (IOException ioException) {
-            System.out.println("Could not create new pack from file " + filename);
-            System.out.println("Try a different file");
+            System.out.println("Could not create new pack from file - " + filename);
+            System.out.println("Try a different file.");
         }
     }
 }
