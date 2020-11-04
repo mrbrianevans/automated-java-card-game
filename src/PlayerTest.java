1d9722fe13801d743c3370a10a3dbf4d7a589725
mrAdamTweedie <61285048+AdamTweedie@users.noreply.github.com>
2020-11-04 12:29:22 +0000

    Update PlayerTest.java

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -4,10 +4,19 @@ import java.io.IOException;
 
 import static org.junit.Assert.*;
 
+/**
+ * PlayerTest class to test all of the functionality and logicality of Player.java. Testing includes playerNumber,
+ * hasWonTrue and hasWonFalse.
+ *
+ * @author 690024916 & 690023094
+ * @version 1.0
+ */
+
 public class PlayerTest {
 
     @Test
     public void getPlayerNumber() throws Exception {
+        // Generate player.
         short playerNumber = (short) Math.round((Math.random() * 100));
         Player player = new Player(playerNumber);
         assertEquals(player.getPlayerNumber(), playerNumber + 1);
@@ -16,7 +25,7 @@ public class PlayerTest {
 
     @Test
     public void hasWonTrue() throws IOException {
-        // this also tests addCard()
+        // This also tests addCard().
         Player player = new Player((short) 1);
         for (int i = 0; i < 4; i++) {
             player.addCard(new Card((short) 2));
@@ -26,7 +35,7 @@ public class PlayerTest {
 
     @Test
     public void hasWonFalse() throws IOException {
-        // this also tests addCard()
+        // This also tests addCard().
         Player player = new Player((short) 1);
         for (int i = 0; i < 4; i++) {
             player.addCard(new Card((short) i));
