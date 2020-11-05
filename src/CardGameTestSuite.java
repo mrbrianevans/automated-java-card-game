ca940b9f944e85e10518292944b8a3004a2e1890
Brian Evans <ebrian101@gmail.com>
2020-11-05 00:12:25 +0000

    Added test suite to run all tests
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

---
+++ src/CardGameTestSuite.java
@@ -0,0 +1,7 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({PlayerTest.class, CardTest.class, CardDeckTest.class, PackTest.class,})
+public class CardGameTestSuite {
+}
